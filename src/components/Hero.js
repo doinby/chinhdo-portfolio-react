48efa6555becf03d53d98a5db6b5b8a53511ab84
Chinh Do <do.inby@gmail.com>
2021-11-30 20:49:17 +1100

    added profile card info & pic

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,35 +2,66 @@
 import React from 'react';
 import {Link as RouterLink} from 'react-router-dom';
 
+import profilePicture from '../../src/images/profile-picture-300.png';
+
 import {
   Card,
   CardContent,
   CardMedia,
   Container,
-  Link,
-  MenuItem,
-  MenuList,
+  List,
+  ListItem,
   Typography,
 } from '@mui/material';
+import {Box} from '@mui/system';
+import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
+
+import './Hero.css';
 
 export default function Hero() {
   return (
-    <Container id='hero-section' component='section'>
-      <Card>
-        <CardMedia component='img' />
+    <Container id='hero-section' component='section' sx={{marginY: 8}}>
+      <Card className='profile-card' sx={{paddingY: 8, paddingX: 8, gap: 2}}>
+        <CardMedia
+          component='img'
+          image={profilePicture}
+          sx={{height: 250, width: 'auto'}}
+        />
         <CardContent>
-          <Typography variant='body1'>
-            Hi, I'm base in a little city called Sydney from the Down Under
-          </Typography>
-          <Typography variant='body1' paragraph={true}>
-            As a
-            <Typography variant='jobTitle' color='primary' sx={{mx: 1}}>
-              Front-end Developer
+          <Box className='intro-paragraph'>
+            <Typography>
+              Hi, I'm base in a little city called Sydney from the{' '}
+              <span className='down-under'>🦘Downunder 🌏</span>
             </Typography>
-            with a keen eye for visual communication, I listen to every little
-            feedback I get from the audience about my design. I'm passionate
-            about developing the highest quality ReactJS products.
-          </Typography>
+            <Typography paragraph={true}>
+              As a
+              <Typography variant='jobTitle' color='primary' sx={{mx: 1}}>
+                Front-end Developer
+              </Typography>
+              with a keen eye for visual communication, I listen to every little
+              feedback I get from the audience about my design. I'm passionate
+              about developing the highest quality ReactJS products.
+            </Typography>
+          </Box>
+          <Box>
+            <Typography>
+              Tech I
+              <FavoriteOutlinedIcon
+                color='primary'
+                sx={{position: 'absolute', ml: 0.25}}
+              />
+              <Typography
+                component='span'
+                sx={{position: 'absolute', ml: 3.25}}
+              >
+                :
+              </Typography>
+            </Typography>
+
+            <List>
+              <ListItem>Js</ListItem>
+            </List>
+          </Box>
         </CardContent>
       </Card>
     </Container>
